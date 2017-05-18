650dee4e2b9d5420b7a8e27e714c72ea3ec420e3
phuengton <phuengton.ch.57@ubu.ac.th>
2017-05-18 13:47:10 +0700

    add template to album

--- resources/views/album.blade.php
+++ resources/views/album.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-  <albums></albums>
+<div class="col-md-12">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h3>ยินดีต้อนรับ</h3></div>
+    <div class="panel-body">
+      I'm an test component!
+    </div>
+  </div>
+</div>
 @stop
