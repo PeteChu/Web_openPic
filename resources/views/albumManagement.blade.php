1fb604da2e142d3c1eeaf1eb62e22a0955f51068
phuengton <phuengton.ch.57@ubu.ac.th>
2017-05-20 17:19:46 +0700

    add delete btn and comfirm btn

--- resources/views/albumManagement.blade.php
+++ resources/views/albumManagement.blade.php
@@ -9,47 +9,48 @@
 
       @php $i = 0; @endphp
       <div class="panel-body">
-        @foreach($photos['photo_path'] as $photo)
-        <div class="col-md-3" align='center'>
-          <form class="" action="/delete" method="post">
-            {{ csrf_field() }}
-            {{ method_field('DELETE') }}
-            <div class="col-md-12 thumbnail">
-                <img src="{{$photo}}" id = "{{$i}}" style='height: 250px; object-fit: container'/><br>
-                <div>
-                  <label for="grid">ขนาด Grid : </label>
-                  <select class="" name="grid[]" id='grid'>
-                    <option value="1" @if($photos['grid'][$i]==1) selected @endif>2x2</option>
-                    <option value="2"@if($photos['grid'][$i]==2) selected @endif>3x3</option>
-                    <option value="3" @if($photos['grid'][$i]==3) selected @endif>3x4</option>
-                  </select>
-                  <button type="submit" class='btn btn-danger'>ลบ</button>
-                </div>
+        <form class="" action="/delete" method="post">
+          @foreach($photos['photo_path'] as $photo)
+          <div class="col-md-3" align='center'>
 
-            </div>
+              {{ csrf_field() }}
+              {{ method_field('DELETE') }}
+              <div class="col-md-12 thumbnail">
+                  <img name='photos_path[]' value='{{$photo}}' src="{{$photo}}" id = "{{$i}}" style='height: 250px; object-fit: container'/><br>
+                  <div>
+                    <label for="grid">ขนาด Grid : </label>
+                    <select class="" name="grid[]" id='grid'>
+                      <option value="1" @if($photos['grid'][$i]==1) selected @endif>2x2</option>
+                      <option value="2"@if($photos['grid'][$i]==2) selected @endif>3x3</option>
+                      <option value="3" @if($photos['grid'][$i]==3) selected @endif>3x4</option>
+                    </select>
+                    <button type="button" class='btn btn-danger'>ลบ</button>
+                  </div>
+              </div>
+          </div>
+          @endforeach
+          <div class="col-md-12" align='center'>
+            <button class='btn btn-success' type="submit" >ยืนยันการเปลี่ยนแปลง</button>
+          </div>
 
-          </form>
-
-        </div>
-
-
-        @endforeach
+        </form>
       </div>
-
     </div>
-
   </div>
-
 
 @stop
 @section('script')
 <script type="text/javascript">
   $(document).ready(function(){
 
+    $('.btn-danger').click( function () {
+      if(confirm('คุณต้องการลบรูปนี้ใช่หรือไม่')){
+        $(this).parent().parent().parent().remove();
+      }
+    })
+
   $('from').click(function () {
     var src = $(this).children().children().attr('src');
-
-
 //     $.ajax({
 //       type: "POST",
 //       url: "./delete/"+src,
@@ -57,6 +58,8 @@
 // });
   // $(this).remove();
   });
+
+
 
   });
 
